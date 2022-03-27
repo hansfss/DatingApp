5f407e01f0a1347d3d5758631e6b9aec0b319690
hans <folkesson.hans@gmail.com>
2022-03-26 21:24:29 -0300

    Walking skeleton - API

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
