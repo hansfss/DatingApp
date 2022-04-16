cdd7b31c3e98eabb6830374fc5ecc1d8f51af4c2
hans <folkesson.hans@gmail.com>
2022-04-16 13:10:01 -0400

    added angular routing applied a new theme

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [//this list contains all the components of the project
-    AppComponent, NavbarComponent, HomeComponent, RegisterComponent
+    AppComponent, NavbarComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +25,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavbarComponent implements OnInit {
 
   model: any = {}
   //methods and properties from accountservice.ts are injected via the constructor
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
 
@@ -20,13 +22,15 @@ export class NavbarComponent implements OnInit {
 
   login() {//call to login method from account service
     this.accountService.login(this.model).subscribe(response => {//userDTO gets returned
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error)
+      console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
